62806fdbcb08d043f36978b7a4dafd0c4431af87
Razvan Paros <razvanparos21@gmail.com>
2025-01-22 00:55:02 +0200

    added env file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRIDlIv0fpbvbBS58j1i1c5fplWRqiKJ8",
-  authDomain: "express-airlines.firebaseapp.com",
-  projectId: "express-airlines",
-  storageBucket: "express-airlines.appspot.com",
-  messagingSenderId: "371846843130",
-  appId: "1:371846843130:web:0e5eb2b13ef95eda371743"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
 export const db = getFirestore(app);
